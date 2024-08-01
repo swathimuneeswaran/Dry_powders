3568b58e3a8b745633cea74c444c0dc4560a4d09
SWATHI M <113039047+swathimuneeswaran@users.noreply.github.com>
2024-08-01 16:56:14 +0530

    commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Company_logo from '../assets/images/Company_logo.png';
 import './Navbar.css'; // Make sure to import the CSS file
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+    document.body.style.marginTop = menuOpen ? '0' : '200px'; // Adjust the value as needed
   };
 
   return (
@@ -23,6 +27,11 @@ const Navbar = () => {
       </nav>
       <div className="hamburger" onClick={toggleMenu}>
         ☰
+      </div>
+      <div className="social-media">
+        <a href="https://www.instagram.com/shreehariglobalfoods/"><InstagramIcon /></a>
+        <a href="https://www.instagram.com/shreehariglobalfoods/"><TwitterIcon /></a>
+        <a href="https://www.instagram.com/shreehariglobalfoods/"><FacebookIcon /></a>
       </div>
       {menuOpen && (
         <div className="blurred-image">
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../component/Navbar";
 import "../styles/Main.css";
 import veg_powder from "../assets/images/veg_powder.jpg";
@@ -16,7 +16,7 @@ import ginger from "../assets/images/ginger.jpg";
 import carrot_flakes from "../assets/images/carrot_flakes.jpg";
 import tomato_flakes from "../assets/images/tomato_flakes.jpg";
 import lemon_powder from "../assets/images/lemon_powder.jpg";
-
+import Company_logo from '../assets/images/Company_logo.png';
 import apple_flakes from "../assets/images/apple_flakes.png";
 import banana_flakes from "../assets/images/banana_flakes.jpg";
 import pineapple_flakes from "../assets/images/pineapple_flakes.png";
@@ -43,10 +43,10 @@ import If_pow1 from "../assets/images/lf_pow1.jpg";
 import If_pow2 from "../assets/images/lf_pow2.jpg";
 import banner2 from "../assets/images/banner2.jpg";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
+import new_poster from "../assets/images/banner2.jpg";
 
 const Main = () => {
   const settings = {
@@ -56,9 +56,39 @@ const Main = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,  // Change to 3000 for 3 seconds or 4000 for 4 seconds
+    autoplaySpeed: 3000, // Change to 3000 for 3 seconds or 4000 for 4 seconds
     arrows: true,
   };
+
+  useEffect(() => {
+    const updateImageSource = () => {
+      const smallScreenImages = document.querySelectorAll(
+        ".small-screen-image"
+      );
+      smallScreenImages.forEach((image) => {
+        if (window.innerWidth <= 700) {
+          image.src = new_poster;
+        } else {
+          if (image.alt.includes("Vegetable Poster")) {
+            image.src = veg_poster;
+          } else if (image.alt.includes("Fruit Poster")) {
+            image.src = fruits_poster;
+          } else if (image.alt.includes("Leaf Poster")) {
+            image.src = leaf_poster;
+          } else if (image.alt.includes("Rose Poster")) {
+            image.src = rose_poster;
+          }
+        }
+      });
+    };
+
+    window.addEventListener("resize", updateImageSource);
+    updateImageSource(); // Initial call
+
+    return () => {
+      window.removeEventListener("resize", updateImageSource);
+    };
+  }, []);
   return (
     <>
       <Navbar />
@@ -76,6 +106,7 @@ const Main = () => {
             </h3>
             <button>Know More</button>
           </div>
+
           <img
             className="image_fruits"
             src="https://i.pinimg.com/564x/6b/47/71/6b4771ba8f59ad439c7344b8ba0ffcbe.jpg"
@@ -161,168 +192,246 @@ const Main = () => {
           </div>
         </div>
       </section>
-       
-       <section style={{marginTop:"100px"}}>
-      <Slider {...settings} >
-      <div >
-        <div className="poster_sec">
-          <img className="image1_veg" src={veg_poster} alt="Vegetable Poster" />
-          <div className="row">
-            <div className="column">
-              <img src={tomato_flakes} alt="Tomato Flakes" />
-              <img src={beetroot_flakes} alt="Beetroot Flakes" />
-            </div>
-            <div className="column">
-              <img src={carrot_flakes} alt="Carrot Flakes" />
+
+      <section style={{ marginTop: "100px" }}>
+        <Slider {...settings}>
+          <div>
+            <div className="poster_sec">
               <img
-                src="https://i.pinimg.com/236x/40/40/4d/40404d7b8bd6aec520416b862f8926dd.jpg"
-                alt="Image 1"
+                className="image1_veg small-screen-image"
+                src={veg_poster}
+                alt="Vegetable Poster"
               />
+              <div className="row">
+                <div className="column ">
+                  <img src={tomato_flakes} alt="Tomato Flakes" />
+                  <img src={beetroot_flakes} alt="Beetroot Flakes" />
+                </div>
+                <div className="column col1 ">
+                  <img src={carrot_flakes} alt="Carrot Flakes" />
+                  <img
+                    src="https://i.pinimg.com/236x/40/40/4d/40404d7b8bd6aec520416b862f8926dd.jpg"
+                    alt="Image 1"
+                  />
+                </div>
+                <div className="column col1">
+                  <img src={peas_powder} alt="Peas Powder" />
+                  <img src={lemon_powder} alt="Lemon Powder" />
+                  <img src={ginger} alt="ginger" />
+                </div>
+                <h3>
+                  Dehydrated fruit powder is made by drying fresh fruits and
+                  then grinding them into a fine powder. This process preserves
+                  the flavor, color, and most of the nutrients of the fresh
+                  fruit while extending its shelf life.<button>Know More</button>
+                </h3>
+              </div>
             </div>
-            <div className="column">
-              <img src={peas_powder} alt="Peas Powder" />
-              <img src={lemon_powder} alt="Lemon Powder" />
-              <img src={ginger} alt="ginger" height="255" />
-              
-            </div>
-            <h3>Dehydrated vegetable powder is made by removing the water content from fresh vegetables through various drying processes, resulting in a concentrated form of the vegetables in powder form. Here are some key points about dehydrated vegetable powder            <button>Know More</button></h3>
-           
           </div>
-        </div>
-      </div>
 
-      <div>
-        <div className="poster_sec">
-          <img className="image1_veg" src={fruits_poster} alt="Fruit Poster" height="700" />
-          <div className="row">
-            <div className="column">
-              <img src={apple_flakes} alt="Apple Flakes" />
-              <img src={banana_flakes} alt="Banana Flakes" height="300" />
-              <img src={orange_powder} height="200" />
-            </div>
-            <div className="column">
-              <img src={jack_flakes} alt="Jackfruit Flakes" />
+          <div>
+            <div className="poster_sec">
               <img
-                src="https://i.pinimg.com/236x/40/40/4d/40404d7b8bd6aec520416b862f8926dd.jpg"
-                alt="Image 1"
-                height="550"
+                className="image1_veg small-screen-image"
+                src={fruits_poster}
+                alt="Fruit Poster"
               />
+              <div className="row">
+                <div className="column">
+                  <img src={apple_flakes} alt="Apple Flakes" />
+                  <img src={banana_flakes} alt="Banana Flakes" />
+                </div>
+                <div className="column col1">
+                  <img src={jack_flakes} alt="Jackfruit Flakes" />
+                  <img
+                    src="https://i.pinimg.com/236x/40/40/4d/40404d7b8bd6aec520416b862f8926dd.jpg"
+                    alt="Image 1"
+                  />
+                </div>
+                <div className="column col1">
+                  <img src={kiwi_powder} alt="Kiwi Powder" />
+                  <img src={pineapple_flakes} alt="Pineapple Flakes" />
+                  <img src={mango_powder} alt="Mango Powder" />
+                </div>
+                <h3>
+                  Dehydrated fruit powder is made by drying fresh fruits and
+                  then grinding them into a fine powder. This process preserves
+                  the flavor, color, and most of the nutrients of the fresh
+                  fruit while extending its shelf life.<button>Know More</button>
+                </h3>
+              </div>
             </div>
-            <div className="column">
-              <img src={kiwi_powder} alt="Kiwi Powder" />
-              <img src={pineapple_flakes} alt="Pineapple Flakes" height="255" />
-              <img src={mango_powder} alt="Mango Powder" height="255" />
-            </div>
-            <h3>Dehydrated fruit powder is made by drying fresh fruits and then grinding them into a fine powder. This process preserves the flavor, color, and most of the nutrients of the fresh fruit while extending its shelf life. Here's a detailed overview of dehydrated fruit powder<button>Know More</button></h3>
           </div>
-        </div>
-      </div>
 
-      <div>
-        <div className="poster_sec">
-          <img className="image1_veg" src={leaf_poster} alt="Leaf Poster" height="700" />
-          <div className="row">
-            <div className="column">
-              <img src={leaf1} alt="Leaf 1" />
-              <img src={leaf2} alt="Leaf 2" height="300" />
-              <img src={leaf3} height="270" />
+          <div>
+            <div className="poster_sec">
+              <img
+                className="image1_veg small-screen-image"
+                src={leaf_poster}
+                alt="Leaf Poster"
+              />
+              <div className="row">
+                <div className="column">
+                  <img src={leaf1} alt="Leaf 1" />
+                  <img src={leaf2} alt="Leaf 2" />
+                  <img src={leaf4} className="col1" alt="Leaf 4" />
+                </div>
+                <div className="column col1">
+                  <img src={leaf4} alt="Leaf 4" />
+                  <img src={leaf5} alt="Leaf 5" />
+                  <img src={If_pow1} alt="Powder 1" />
+                </div>
+                <div className="column col1">
+                  <img src={If_pow1} alt="Powder 1" />
+                  <img src={If_pow2} alt="Powder" />
+                  <img src={leaf1} alt="Leaf 1" />
+                </div>
+                <h3>
+                  Dehydrated leaf powder is produced by drying and grinding
+                  various types of edible leaves into a fine powder. This
+                  process preserves the nutritional benefits and flavor of the
+                  fresh leaves while significantly extending their shelf life.
+                 
+                  <button>Know More</button>
+                </h3>
+              </div>
             </div>
-            <div className="column">
-              <img src={leaf4} alt="Leaf 4" />
-              <img src={leaf5} alt="Leaf 5" height="550" />
-            </div>
-            <div className="column">
-              <img src={If_pow1} alt="Powder 1" />
-              <img src={If_pow2} alt="Powder 2" height="255" />
-              <img src={leaf1} alt="Leaf 1" height="300" />
-            </div>
-            <h3>Dehydrated leaf powder is produced by drying and grinding various types of edible leaves into a fine powder. This process preserves the nutritional benefits and flavor of the fresh leaves while significantly extending their shelf life. Here’s an in-depth look at dehydrated leaf powder<button>Know More</button></h3>
           </div>
-        </div>
-      </div>
 
-      <div>
-        <div className="poster_sec">
-          <img className="image1_veg" src={rose_poster} alt="Rose Poster" height="700" />
-          <div className="row">
-            <div className="column">
-              <img src={rose1} alt="Rose 1" />
-              <img src={rose2} alt="Rose 2" height="300" />
-              <img src={rose3} height="190" />
+          <div>
+            <div className="poster_sec">
+              <img
+                className="image1_veg small-screen-image"
+                src={rose_poster}
+                alt="Rose Poster"
+              />
+              <div className="row">
+                <div className="column">
+                  <img src={rose1} alt="Rose 1" />
+                  <img src={rose2} alt="Rose 2" />
+                  <img src={rose3} />
+                </div>
+                <div className="column col1">
+                  <img src={rose4} alt="Rose 4" />
+                  <img src={rose5} alt="Rose 5" />
+                </div>
+                <div className="column col1">
+                  <img src={rose6} alt="Rose 6" />
+                  <img src={rose7} alt="Rose 7" />
+                  <img src={rose8} alt="Rose 8" />
+                </div>
+                <h3>
+                  {" "}
+                  Dehydrated rose powder is made by drying and grinding rose
+                  petals into a fine powder. This process preserves the
+                  aromatic, nutritional, and therapeutic properties of the
+                  roses. Here’s an in-depth look at dehydrated rose powder
+                  <button>Know More</button>
+                </h3>
+              </div>
             </div>
-            <div className="column">
-              <img src={rose4} alt="Rose 4" />
-              <img src={rose5} alt="Rose 5" height="530" />
-            </div>
-            <div className="column">
-              <img src={rose6} alt="Rose 6" />
-              <img src={rose7} alt="Rose 7" height="255" />
-              <img src={rose8} alt="Rose 8" height="220" />
-            </div>
-            <h3> Dehydrated rose powder is made by drying and grinding rose petals into a fine powder. This process preserves the aromatic, nutritional, and therapeutic properties of the roses. Here’s an in-depth look at dehydrated rose powder<button>Know More</button></h3>
           </div>
+        </Slider>
+      </section>
+
+      <section>
+        <h1
+          style={{ textAlign: "center", fontSize: "39px", marginTop: "80px" }}
+        >
+          Health Benefits
+        </h1>
+        <div className="quality">
+          <div>
+            <h3>Health Benefits of Dehydrated Powder</h3>
+            <p>
+              Dehydrated fruit, vegetable, leaf, and rose powders are
+              nutrient-dense, providing essential vitamins, minerals, and
+              antioxidants that support immune health and overall wellness. They
+              also offer anti-inflammatory and digestive benefits, promoting a
+              healthy body and skin.
+            </p>
+          </div>
+          <img src="https://img.ws.mms.shopee.ph/sg-11134201-7rblq-llujjlzattuie5" />
+          <img src="https://arealcc.com/images/products/1707480342.webp" />
+          <img src="https://www.osheaherbals.com/cdn/shop/articles/benefits-of-rose-powder-for-skin.jpg?v=1702476662" />
         </div>
+      </section>
+
+      <section className="newsletter-section">
+        <div className="overlay">
+          <h2>
+            Unlock the Power of Nature with Our Dehydrated Powders. Join Now and
+            Save!
+          </h2>
+          <input type="text" placeholder="Enter your email" />
+          <button>Send Email</button>
+        </div>
+        <img
+          src="https://i0.wp.com/choicetitle.com/wp-content/uploads/2023/09/AdobeStock_gold-pineapples-black-right-side-1.jpg?resize=1180%2C530&ssl=1"
+          alt="Newsletter Banner"
+        />
+      </section>
+
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-logo">
+          <img src={Company_logo} alt="Company Logo" />
+            <h2>Shree Hari Global Foods</h2>
+            <p>
+              is one of the leading fresh fruit vegetables, spices and grains
+              export Company from INDIA.
+            </p>
+          </div>
+          <div className="footer-links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About Our Company</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+              <li>
+                <a href="#">Payment Policy</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Terms and Conditions</a>
+              </li>
+              <li>
+                <a href="#">Cancellation and Refund</a>
+              </li>
+              <li>
+                <a href="#">Shipping and Delivery Policy</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-contact">
+            <h3>Contact Us</h3>
+            <p>
+              16, Parmeshwar Park, Rail Nagar Main Road, Rajkot, Gujrat - 360001
+            </p>
+            <p>+91-8870292325</p>
+            <p>
+              <a href="mailto:shreehariglobalfoods@gmail.com">
+                shreehariglobalfoods@gmail.com
+              </a>
+            </p>
+          </div>
+          {/* <div className="footer-social">
+            <h3>Follow Us</h3>
+          </div> */}
+        
+        </div>
+        <div className="footer-bottom">
+        <p>&copy; 2024 Shree Hari Global Foods. All rights reserved.</p>
       </div>
-    </Slider>
-    </section>
-
-    <section >
-      <h1 style={{textAlign:"center",fontSize:"39px",marginTop:"80px"}}>Health Benefits</h1>
-      <div className="quality">
-      <div>
-        <h3>Health Benefits of Dehydrated Powder</h3>
-        <p>Dehydrated fruit, vegetable, leaf, and rose powders are nutrient-dense, providing essential vitamins, minerals, and antioxidants that support immune health and overall wellness. They also offer anti-inflammatory and digestive benefits, promoting a healthy body and skin.</p>
-      </div>
-      <img src="https://img.ws.mms.shopee.ph/sg-11134201-7rblq-llujjlzattuie5" />
-      <img src="https://arealcc.com/images/products/1707480342.webp" />
-      <img src="https://www.osheaherbals.com/cdn/shop/articles/benefits-of-rose-powder-for-skin.jpg?v=1702476662" />
-      </div>
-    </section>
-
-
-    <section className="newsletter-section">
-  <div className="overlay">
-    <h2>Unlock the Power of Nature with Our Dehydrated Powders. Join Now and Save!</h2>
-    <input type="text" placeholder="Enter your email" />
-    <button>Send Email</button>
-  </div>
-  <img src="https://i0.wp.com/choicetitle.com/wp-content/uploads/2023/09/AdobeStock_gold-pineapples-black-right-side-1.jpg?resize=1180%2C530&ssl=1" alt="Newsletter Banner" />
-</section>
-
-
-<footer className="footer">
-  <div className="footer-container">
-    <div className="footer-logo">
-      <h2>Shree Hari Global Foods</h2>
-      <p>is one of the leading fresh fruit vegetables, spices and grains export Company from INDIA.</p>
-    </div>
-    <div className="footer-links">
-      <h3>Quick Links</h3>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About Our Company</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li><a href="#">Payment Policy</a></li>
-        <li><a href="#">Privacy Policy</a></li>
-        <li><a href="#">Terms and Conditions</a></li>
-        <li><a href="#">Cancellation and Refund</a></li>
-        <li><a href="#">Shipping and Delivery Policy</a></li>
-      </ul>
-    </div>
-    <div className="footer-contact">
-      <h3>Contact Us</h3>
-      <p>16, Parmeshwar Park, Rail Nagar Main Road, Rajkot, Gujrat - 360001</p>
-      <p>+91-8870292325</p>
-      <p><a href="mailto:shreehariglobalfoods@gmail.com">shreehariglobalfoods@gmail.com</a></p>
-    </div>
-    <div className="footer-social">
-      <h3>Follow Us</h3>
-     
-    </div>
-  </div>
-</footer>
-
-
+      </footer>
     </>
   );
 };
